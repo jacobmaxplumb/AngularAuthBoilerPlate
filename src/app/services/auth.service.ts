61481cc159ea9ab00a30422990e9056cd6c6a497
Jacob Plumb <jacobplumb@Jacobs-MacBook-Pro.local>
2019-04-30 23:34:00 -0400

    added function in auth service to get token as observable

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  get isLoggedIn() {
-    return localStorage.getItem('token') !== undefined;
+  private isLoggedIn = false;
+
+  constructor(private http: HttpClient) { }
+
+  getTokenObservable(username: string, password: string): Observable<string> {
+    return this.http.get<string>(
+      `${environment.apiUrl}/token/${username}/${password}`
+      );
   }
 }
